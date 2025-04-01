5ab7fbce0857b0edf3b3e977620ba7fb24e26e3a
developeeer-level1 <yak.yakovochka@gmail.com>
2025-04-02 00:43:32 +0100

    ф

--- dz5/Program.cs
+++ dz5/Program.cs
@@ -1,12 +1,82 @@
 ﻿using System;
+using System.Linq;
 
 namespace dz5
 {
     class Program
     {
+        //Task1
+        //static void DisplaySquare(int sideLength, char symbol)
+        //{
+        //    for (int i = 0; i < sideLength; i++)
+        //    {
+        //        for (int j = 0; j < sideLength; j++)
+        //        {
+        //            Console.Write(symbol);
+        //        }
+        //        Console.WriteLine();
+        //    }
+        //}
+        //Task2
+        //static bool IsPalindrome(int number)
+        //{
+        //    string numStr = number.ToString();
+        //    string reversedStr = string.Join("", numStr.Reverse());
+        //    return numStr == reversedStr;
+        //}
+        //Task3
+        //static int[] FilterArray(int[] originalArray, int[] filterArray)
+        //{
+        //    int count = 0;
+        //    for (int i = 0; i < originalArray.Length; i++)
+        //    {
+        //        bool isFiltered = false;
+        //        for (int j = 0; j < filterArray.Length; j++)
+        //        {
+        //            if (originalArray[i] == filterArray[j])
+        //            {
+        //                isFiltered = true;
+        //                break;
+        //            }
+        //        }
+        //        if (!isFiltered)
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //
+        //    int[] resultArray = new int[count];
+        //    int index = 0;
+        //    for (int i = 0; i < originalArray.Length; i++)
+        //    {
+        //        bool isFiltered = false;
+        //        for (int j = 0; j < filterArray.Length; j++)
+        //        {
+        //            if (originalArray[i] == filterArray[j])
+        //            {
+        //                isFiltered = true;
+        //                break;
+        //            }
+        //        }
+        //        if (!isFiltered)
+        //        {
+        //            resultArray[index++] = originalArray[i];
+        //        }
+        //    }
+        //    return resultArray;
+        //}
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Task1
+            //DisplaySquare(5, '#');
+
+            //Task2
+            //Console.WriteLine(IsPalindrome(1221));
+            //Console.WriteLine(IsPalindrome(3443));
+            //Console.WriteLine(IsPalindrome(7854));
+
         }
+
     }
+
 }
