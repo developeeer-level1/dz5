b349b6f3e76b702ec2845abf842fbf6b1cf23b5f
developeeer-level1 <yak.yakovochka@gmail.com>
2025-04-03 21:03:49 +0100

    a

---
+++ dz5/Money.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz5
+{
+    internal class Money
+    {
+        public int Hryvnas { get; set; }
+        public int Kopecks { get; set; }
+        public Money(int hryvnas, int kopecks)
+        {
+            Hryvnas = hryvnas + kopecks / 100;
+            Kopecks = kopecks % 100;
+            Normalize();
+        }
+        private void Normalize()
+        {
+            if (Kopecks < 0 || Hryvnas < 0)
+            {
+                throw new WrongEntering("Bankrupt: the amount cannot be taken into account");
+            }
+        }
+        public static Money operator +(Money a, Money b)
+        {
+            return new Money(a.Hryvnas + b.Hryvnas, a.Kopecks + b.Kopecks);
+        }
+        public static Money operator -(Money a, Money b)
+        {
+            return new Money(a.Hryvnas - b.Hryvnas, a.Kopecks - b.Kopecks);
+        }
+        public static Money operator *(Money a, int multiplier)
+        {
+            return new Money(a.Hryvnas * multiplier, a.Kopecks * multiplier);
+        }
+        public static Money operator /(Money a, int divisor)
+        {
+            if (divisor == 0) throw new DivideByZeroException("Dividing by zero is impossible");
+            int totalKopecks = (a.Hryvnas * 100 + a.Kopecks) / divisor;
+            return new Money(totalKopecks / 100, totalKopecks % 100);
+        }
+        public static bool operator ==(Money a, Money b)
+        {
+            return a.Hryvnas == b.Hryvnas && a.Kopecks == b.Kopecks;
+        }
+        public static bool operator !=(Money a, Money b)
+        {
+            return !(a == b);
+        }
+        public static bool operator >(Money a, Money b)
+        {
+            return (a.Hryvnas * 100 + a.Kopecks) > (b.Hryvnas * 100 + b.Kopecks);
+        }
+        public static bool operator <(Money a, Money b)
+        {
+            return (a.Hryvnas * 100 + a.Kopecks) < (b.Hryvnas * 100 + b.Kopecks);
+        }
+        public static Money operator ++(Money a)
+        {
+            return new Money(a.Hryvnas, a.Kopecks + 1);
+        }
+        public static Money operator --(Money a)
+        {
+            return new Money(a.Hryvnas, a.Kopecks - 1);
+        }
+        public override string ToString()
+        {
+            return $"{Hryvnas} UAH {Kopecks} kopecks.";
+        }
+    }
+}
--- dz5/Program.cs
+++ dz5/Program.cs
@@ -5,78 +5,32 @@ namespace dz5
 {
     class Program
     {
-        //Task1
-        //static void DisplaySquare(int sideLength, char symbol)
-        //{
-        //    for (int i = 0; i < sideLength; i++)
-        //    {
-        //        for (int j = 0; j < sideLength; j++)
-        //        {
-        //            Console.Write(symbol);
-        //        }
-        //        Console.WriteLine();
-        //    }
-        //}
-        //Task2
-        //static bool IsPalindrome(int number)
-        //{
-        //    string numStr = number.ToString();
-        //    string reversedStr = string.Join("", numStr.Reverse());
-        //    return numStr == reversedStr;
-        //}
-        //Task3
-        //static int[] FilterArray(int[] originalArray, int[] filterArray)
-        //{
-        //    int count = 0;
-        //    for (int i = 0; i < originalArray.Length; i++)
-        //    {
-        //        bool isFiltered = false;
-        //        for (int j = 0; j < filterArray.Length; j++)
-        //        {
-        //            if (originalArray[i] == filterArray[j])
-        //            {
-        //                isFiltered = true;
-        //                break;
-        //            }
-        //        }
-        //        if (!isFiltered)
-        //        {
-        //            count++;
-        //        }
-        //    }
-        //
-        //    int[] resultArray = new int[count];
-        //    int index = 0;
-        //    for (int i = 0; i < originalArray.Length; i++)
-        //    {
-        //        bool isFiltered = false;
-        //        for (int j = 0; j < filterArray.Length; j++)
-        //        {
-        //            if (originalArray[i] == filterArray[j])
-        //            {
-        //                isFiltered = true;
-        //                break;
-        //            }
-        //        }
-        //        if (!isFiltered)
-        //        {
-        //            resultArray[index++] = originalArray[i];
-        //        }
-        //    }
-        //    return resultArray;
-        //}
         static void Main(string[] args)
         {
-            //Task1
-            //DisplaySquare(5, '#');
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            try
+            {
+                Money m1 = new Money(10, 50);
+                Money m2 = new Money(5, 25);
 
-            //Task2
-            //Console.WriteLine(IsPalindrome(1221));
-            //Console.WriteLine(IsPalindrome(3443));
-            //Console.WriteLine(IsPalindrome(7854));
-
+                Console.WriteLine("First number: " + m1);
+                Console.WriteLine("Another number: " + m2);
+                Console.WriteLine("Amount: " + (m1 + m2));
+                Console.WriteLine("Retail: " + (m1 - m2));
+                Console.WriteLine("Multiply by 2: " + (m1 * 2));
+                Console.WriteLine("Division by 2: " + (m1 / 2));
+                Console.WriteLine("Increase by 1 copy: " + (++m1));
+                Console.WriteLine("Change by 1 copy: " + (--m2));
+                Console.WriteLine("Is one better than another? " + (m1 > m2));
+                Console.WriteLine("Is one better than the other? " + (m1 < m2));
+                Console.WriteLine("What are the regions?" + (m1 == m2));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Request: " + ex.Message);
+            }
+            Console.ReadLine();
         }
-
     }
 
 }
